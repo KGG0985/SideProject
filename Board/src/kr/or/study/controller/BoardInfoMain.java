b0dc0b775c18ac778243c7ebe7794a6d67c9db64
DESKTOP-CAUII4V\admin <kgg0985@gmail.com>
2022-06-22 16:17:30 +0900

    Base

--- Board/src/kr/or/study/controller/BoardInfoMain.java
+++ Board/src/kr/or/study/controller/BoardInfoMain.java
@@ -1,7 +1,6 @@
 package kr.or.study.controller;
 
-import java.util.Scanner;
 
 public class BoardInfoMain {
-	private Scanner scan = new Scanner(System.in);
+	
 }
--- Board/src/kr/or/study/util/MybatisUtil.java
+++ Board/src/kr/or/study/util/MybatisUtil.java
@@ -1,4 +1,4 @@
-package kr.or.study.board.util;
+package kr.or.study.util;
 
 import java.io.IOException;
 import java.io.Reader;
