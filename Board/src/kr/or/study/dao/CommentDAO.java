24133dc32a6fdd1d054bfa0c75262c05fc86455b
DESKTOP-CAUII4V\admin <kgg0985@gmail.com>
2022-06-27 19:17:58 +0900

    commentRead

--- Board/src/kr/or/study/dao/CommentDAO.java
+++ Board/src/kr/or/study/dao/CommentDAO.java
@@ -8,5 +8,10 @@ public interface CommentDAO {
 	public List<CommentVO> getCommentAll(CommentVO cv);
 	
 	public int writeComment(CommentVO cv);
-
+	
+	public int updateComment(CommentVO cv);
+	
+	public int deleteComment(String comNo);
+	
+	public boolean checkComment(String comNo);
 }
